87cc02fb12123be276084d0c2df325a8aa73b085
Muhammad Hariss <muhammad.hariss@integrationxperts.com>
2026-05-13 12:29:47 +0500

    changes for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  basePath: "/Admin",
+  trailingSlash: true,
 };
 
 export default nextConfig;
